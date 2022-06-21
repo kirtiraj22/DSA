3a5068be72b27d1e77ca987a57d6b9890167e8bb
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-06-21 13:12:05 +0530

    Update power_of_a_and_b.cpp

--- FUNCTIONS/power_of_a_and_b.cpp
+++ FUNCTIONS/power_of_a_and_b.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
+int answer(int base,int power){            //making a function to find power
+    int ans = 1;
+    for(int i = 0 ; i < power ; i++){
+        ans = ans * base;
+    }
+    return ans;
+}
+
 int main(){
     int base,power;
     cout << "Enter base: ";
     cin >> base;
     cout << "Enter power: ";
     cin >> power;
-    int ans = 1;
-
-    for(int i = 0 ; i < power ; i++){
-        ans = ans * base;
-    }
-    cout << "answer is " << ans;
+   
+    cout << "answer is " << answer(base,power);
+    return 0;
 }
 
 /*output : 
