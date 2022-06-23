4944248cc327eb598dfd2faad2622834be765ce2
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-06-23 23:22:55 +0530

    Create min_and_max.cpp

---
+++ ARRAYS/min_and_max.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <limits.h>       //using limits.h to include int_max and int_min
+
+using namespace std;
+
+//minimum and maximum in array
+
+int getmax(int arr[],int size){
+
+    int max = INT_MIN;          //storing minimum value of integer to int max
+
+    for(int i = 0; i < size; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int getmin(int arr[],int size){
+
+    int min = INT_MAX;         //storing maximum value in int min
+
+    for(int i = 0; i < size; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+
+int main(){
+    int size;
+    cin >> size;
+
+    int arr[100];           /*instead of putting size variable inside arr
+                            i.e arr[size] , use large number like arr[100]*/
+
+    for(int i = 0; i < size ;i++){
+        cin >> arr[i];
+    }
+
+    cout << "maximum is : " << getmax(arr,size) << endl;
+    cout << "minimum is : " << getmin(arr,size) << endl;
+    return 0; 
+}
