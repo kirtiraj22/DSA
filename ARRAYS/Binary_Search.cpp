e4e30bcae8317a5d56e5304ee21a4a9730dd27d2
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-07-03 18:54:12 +0530

    Create Binary_Search.cpp

---
+++ ARRAYS/Binary_Search.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int PrintArray(int arr[],int size){
+  for(int i = 0; i < size; i++){
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+//eg : 2 , 4 , 5 , 6 , 8
+int BinarySearch(int arr[],int size,int target){
+  int first = 0;
+  int last = size-1;
+
+  while(first <= last){  
+    int middle = (first + last )/ 2;
+    if(arr[middle] == target){
+      return middle;
+    }
+    else if(arr[middle] < target){
+      first = middle+1;
+    }
+    else{
+      last = middle - 1;
+    }
+  }
+  return -1;
+}
+
+int main(){
+  int size,arr[100],target;
+  cin >> size;
+  for(int i = 0; i < size; i++){
+    cin >> arr[i];
+  }
+  cin >> target;
+  sort(arr,arr+size);
+  cout << "sorted array is : " << endl;
+  PrintArray(arr,size);
+  int ans = BinarySearch(arr,size,target);
+  cout << "element found at index " << ans;
+  return 0;
+}
+
+
+//TIME COMPLEXITY : O(log n)
+
+/*
+INPUT :
+5
+1 3 2 5 4
+5
+OUTPUT :
+sorted array is : 
+1 2 3 4 5
+element found at : 4
+*/
