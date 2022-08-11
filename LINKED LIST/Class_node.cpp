513ea4088df494031174140ed76eb6cb972ab599
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-08-11 12:35:27 +0530

    Create Class_node.cpp

---
+++ LINKED LIST/Class_node.cpp
@@ -0,0 +1,16 @@
+class Node{
+public:
+    int key;
+    int data;
+    Node* next;
+
+    Node(){
+        key = 0;
+        data = 0;
+        next = NULL;
+    }
+    Node(int k, int d){     //parameterized constructor
+        key = k;
+        data = d;
+    }
+};
